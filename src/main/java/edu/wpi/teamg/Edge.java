80bc93c9e5ca640ce278c3c22b8df9a9826d3dae
Thomas3737 <thomas.mcdonagh037@gmail.com>
2023-03-25 14:04:44 -0400

    CSV parser start

--- src/main/java/edu/wpi/teamg/Edge.java
+++ src/main/java/edu/wpi/teamg/Edge.java
@@ -2,9 +2,10 @@ package edu.wpi.teamg;
 import java.lang.Math;
 
 public class Edge {
-    private String edgeID;
-    private Node startNode;
-    private Node endNode;
+  private String edgeID;
+  private Node startNode;
+  private Node endNode;
+
 
     public Edge(String ID, Node start, Node end){
         edgeID=ID;
@@ -23,4 +24,14 @@ public class Edge {
 
     }
 
+  public Edge(String ID, Node start, Node end) {
+    edgeID = ID;
+    startNode = start;
+    endNode = end;
+  }
+>>>>>>> Stashed changes
+
+  public int distance(Node A, Node B) {
+    return 0;
+  }
 }
