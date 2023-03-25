65fee7dc720bc04ff73f04eba0784dd03677da08
da-munchies24 <donbothermeatall16@gmail.com>
2023-03-25 14:06:18 -0400

    Update Edge.java

--- src/main/java/edu/wpi/teamg/Edge.java
+++ src/main/java/edu/wpi/teamg/Edge.java
@@ -2,36 +2,25 @@ package edu.wpi.teamg;
 import java.lang.Math;
 
 public class Edge {
-  private String edgeID;
-  private Node startNode;
-  private Node endNode;
+    private String edgeID;
+    private Node startNode;
+    private Node endNode;
 
 
-    public Edge(String ID, Node start, Node end){
-        edgeID=ID;
-        startNode=start;
-        endNode=end;
+    public Edge(String ID, Node start, Node end) {
+        edgeID = ID;
+        startNode = start;
+        endNode = end;
     }
 
-    public double distance(Node A, Node B){
-        double x1= A.getNodeX();
-        double x2= B.getNodeX();
-        double y1= A.getNodeY();
-        double y2= B.getNodeY();
-        double distance= Math.sqrt((Math.pow(x2-x1,2)+Math.pow(y2-y1,2)));
+    public double distance(Node A, Node B) {
+        double x1 = A.getNodeX();
+        double x2 = B.getNodeX();
+        double y1 = A.getNodeY();
+        double y2 = B.getNodeY();
+        double distance = Math.sqrt((Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
         return distance;
 
 
     }
-
-  public Edge(String ID, Node start, Node end) {
-    edgeID = ID;
-    startNode = start;
-    endNode = end;
-  }
->>>>>>> Stashed changes
-
-  public int distance(Node A, Node B) {
-    return 0;
-  }
 }
