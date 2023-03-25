4b668696eb81d1a39fc666fd08078a9b9eb044ac
da-munchies24 <donbothermeatall16@gmail.com>
2023-03-25 13:39:40 -0400

    updated the edge class

---
+++ src/main/java/edu/wpi/teamg/Edge.java
@@ -0,0 +1,12 @@
+package edu.wpi.teamg;
+
+public class Edge {
+    private String edgeID;
+    private Node startNode;
+    private Node endNode;
+
+    public int distance(Node A, Node B){
+
+    }
+
+}
