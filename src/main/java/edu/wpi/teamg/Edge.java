f6d4101cd7ad756305f78bb4814a0e4e8235798f
da-munchies24 <donbothermeatall16@gmail.com>
2023-03-25 13:43:05 -0400

    Update Edge.java

--- src/main/java/edu/wpi/teamg/Edge.java
+++ src/main/java/edu/wpi/teamg/Edge.java
@@ -5,7 +5,14 @@ public class Edge {
     private Node startNode;
     private Node endNode;
 
+    public Edge(String ID, Node start, Node end){
+        edgeID=ID;
+        startNode=start;
+        endNode=end;
+    }
+
     public int distance(Node A, Node B){
+        return 0;
 
     }
 
