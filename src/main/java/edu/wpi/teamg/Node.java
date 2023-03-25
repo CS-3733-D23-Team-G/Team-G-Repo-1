dc24d4e50baf8232d44891f873cb019760a6f53a
Thomas3737 <thomas.mcdonagh037@gmail.com>
2023-03-25 13:43:36 -0400

    Update Node.java

--- src/main/java/edu/wpi/teamg/Node.java
+++ src/main/java/edu/wpi/teamg/Node.java
@@ -1,4 +1,28 @@
 package edu.wpi.teamg;
 
 public class Node {
+
+
+    private String NodeID;
+    private int nodeX;
+    private int nodeY;
+    private String floor;
+    private String building;
+    private String longName;
+    private String shortName;
+
+    public Node(String NodeID, int nodeX, int nodeY, String floor, String building, String longName, String shortName){
+        this.NodeID = NodeID;
+        this.nodeX = nodeX;
+        this.nodeY = nodeY;
+        this.floor = floor;
+        this.building = building;
+        this.longName = longName;
+        this.shortName = shortName;
+
+    }
+
+
+
+
 }
