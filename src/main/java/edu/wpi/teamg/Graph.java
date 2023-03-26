50150206175faab78fb77a384eb0e4a44ceddae0
da-munchies24 <donbothermeatall16@gmail.com>
2023-03-26 18:07:49 -0400

    Update Graph.java

--- src/main/java/edu/wpi/teamg/Graph.java
+++ src/main/java/edu/wpi/teamg/Graph.java
@@ -5,7 +5,7 @@ public class Graph {
   private Edge[] E;
   public int[][] createWeightedAdj(Edge[] E1, Node[] N1){
     int[][] A1= new int[N1.length][N1.length];
-    // populate the adj matrix with zeros or inf change as nessary
+    // populate the adj matrix with zeros or inf (change as needed)
       for(int i=0; i<N1.length; i++){
           for(int j=0; j<N1.length; j++){
               A1[i][j]=0;
@@ -31,7 +31,7 @@ public class Graph {
           }
         }
         //we then calulate the distance between the two nodes and put it in the A1[][] array
-        //since the adj matrix is sysmetric the number in the rowxcolum and columxrow will be the same. 
+        //since the adj matrix is sysmetric the number in the rowxcolum and columxrow will be the same.
         A1[vertice_numS][vertice_numE]=E1[i].distance(start,end);
         A1[vertice_numE][vertice_numS]=E1[i].distance(start,end);
       }
