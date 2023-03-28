1caa999e97f2c0a1a45a926609d31d75694cfc8d
rganguli0753 <111610444+rganguli0753@users.noreply.github.com>
2023-03-27 23:29:47 -0400

    Figured out how to obscure login
    Obscured the login gdb

--- src/main/java/edu/wpi/teamg/Gdb.java
+++ src/main/java/edu/wpi/teamg/Gdb.java
@@ -1,6 +1,13 @@
 package edu.wpi.teamg;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import org.yaml.snakeyaml.Yaml;
 
 public class Gdb {
   static Connection connection;
@@ -9,7 +16,7 @@ public class Gdb {
     try {
       connection =
           DriverManager.getConnection(
-              "jdbc:postgresql://database.cs.wpi.edu:5432/teamgdb", "teamg", "teamg70");
+              getDBCreds().get(0), getDBCreds().get(1), getDBCreds().get(2));
     } catch (SQLException e) {
       //      e.printStackTrace();
       System.err.println("SQL exception");
@@ -109,5 +116,22 @@ public class Gdb {
 
   public static Connection getConnection() {
     return connection;
+  }
+
+  private List<String> getDBCreds() {
+    List<String> creds = new LinkedList<>();
+    try {
+      InputStream is = new FileInputStream("creds.yml");
+
+      Yaml yaml = new Yaml();
+      Map<String, Object> data = yaml.load(is);
+      creds.add(data.get("url").toString());
+      creds.add(data.get("username").toString());
+      creds.add(data.get("password").toString());
+
+      return creds;
+    } catch (FileNotFoundException e) {
+      return null;
+    }
   }
 }
